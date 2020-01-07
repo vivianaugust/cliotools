827afaca313f377b2e6937b7837e71cd39e070aa
Logan Pearce <loganpearce55@gmail.com>
2020-01-07 12:35:15 -0700

    altered findstars to be agnostic to the number of stars in image.

--- cliotools/bditools.py
+++ cliotools/bditools.py
@@ -1,69 +1,32 @@
-def findstars(imstamp, scienceimage_filename, xca, yca, boxsizex = 100, boxsizey = 100, threshold = 1e4, radius = 20):
-    """Find the subpixel location of stars A and B in a single clio BDI image.
+def daostarfinder(scienceimage, x, y, boxsize = 100, threshold = 1e4, fwhm = 10):
+    """Find the subpixel location of a single star in a single clio BDI image.
        Parameters:
        -----------
-       imstamp : 2d array
+       image : 2d array
            boxsizex by boxsizey substamp image of a reference psf for cross correlation
        scienceimage_filename : string
            path to science image
-       xca, yca : int
-           integer pixel locations of star A
+       x, y : int
+           integer pixel locations of star
        boxsize : int
            size of box to draw around star psfs for DAOStarFinder
+       threshold : int
+           threshold keyword for StarFinder
+       fwhm : int
+           fwhm keyword for StarFinder
            
        Returns:
        --------
-       xca_subpix, yca_subpix, xcb_subpix, ycb_subpix : flt
-           subpixel location of star A and star B
+       x_subpix, y_subpix : flt
+           subpixel location of star
     """
-    from scipy import signal, ndimage
     from photutils import DAOStarFinder
-    from astropy.io import fits
     import numpy as np
-    # Supress warnings when failing to find point sources
-    import warnings
-    warnings.filterwarnings("ignore")
-    
-    # Open science target image:
-    image2 = fits.getdata(scienceimage_filename)
-    if len(image2.shape) == 3:
-        image2 = image2[0]
-    image2 = ndimage.median_filter(image2, 3)
-    # Use cross-correlation to find int(y,x) of star A (brightest star) in image:
-    corr = signal.correlate2d(image2, imstamp, boundary='symm', mode='same')
-    y, x = np.unravel_index(np.argmax(corr), corr.shape)
-    # Define the star finder parameters:
-    # The settings fwhm = 10.0 threshold = 1e4 are best for distinguishing good psf
-    # from bad psf, by trial and error.
-    daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
-    # Find sub-pixel location of star A in science image by using DAOStarFinder on a postagestamp centered at
-    # cross-correlation x,y position results:
-    sources = daofind(image2[np.int_(y-boxsizey):np.int_(y+boxsizey),np.int_(x-boxsizex):np.int_(x+boxsizex)])
-    
-    # If it finds too many sources, keep uping the search threshold until there is only one:
-    while len(sources) > 1:
-        threshold += 500
-        daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
-        sources = daofind(image2[np.int_(y-boxsizey):np.int_(y+boxsizey),np.int_(x-boxsizex):np.int_(x+boxsizex)])
-        
-    # Get image location of star A:
-    xca_subpix, yca_subpix = (x-boxsizex+sources['xcentroid'])[0], (y-boxsizey+sources['ycentroid'])[0]
-    
-    # Make a mask around the brightest star on the cross-correlation image:
-    # Make a meshgrid of the image centered at star A:
-    xx,yy = np.meshgrid(np.arange(image2.shape[1])-xca_subpix,np.arange(image2.shape[0])-yca_subpix)
-    # Make an array of the distances of each pixel from that center:
-    rA=np.hypot(xx,yy)
-    # Mask wherever that distance is less than the set radius:
-    corr_masked = corr.copy()
-    # Set those pixels to zero:
-    corr_masked[np.where((rA < radius))] = 0
-    # Now find the new highest correlation which should be the other star:
-    y2, x2 = np.unravel_index(np.argmax(corr_masked), corr.shape)
-
+    image2 = scienceimage
+    # The settings fwhm = 10.0 seems to be good for clio
     # Define box around source B:
-    ymin, ymax = y2-boxsizey, y2+boxsizey
-    xmin, xmax = x2-boxsizex, x2+boxsizex
+    ymin, ymax = y-boxsize, y+boxsize
+    xmin, xmax = x-boxsize, x+boxsize
     # Correct for sources near image edge:
     if ymin < 0:
         ymin = 0
@@ -73,44 +36,122 @@ def findstars(imstamp, scienceimage_filename, xca, yca, boxsizex = 100, boxsizey
         xmin = 0
     if xmax > 1024:
         xmax = 1024
-    # Use DAOFind to find subpixel location of B:
+    # Use DAOStarFinder to find subpixel locations:
     # If the threshold is too high and it can't find a point source, lower the threshold
-    # until ~ 1e3, then declare the image might be bad.
+    # until ~ 5e2, then declare failure.
     try:
-        daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
+        #print('threshold =',threshold)
+        daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold) 
         sources = daofind(image2[np.int_(ymin):np.int_(ymax),\
                                      np.int_(xmin):np.int_(xmax)])
-        xcb_subpix, ycb_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
+        x_subpix, y_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
+        # If it finds too many sources, keep uping the search threshold until there is only one:
+        while len(sources) > 1:
+            threshold += 500
+            daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
+            sources = daofind(image2[np.int_(y-boxsize):np.int_(y+boxsize),np.int_(x-boxsize):np.int_(x+boxsize)])
+            x_subpix, y_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
     except:
         try:
             threshold=1e3
-            daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
+            #print('threshold =',threshold)
+            daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold) 
             sources = daofind(image2[np.int_(ymin):np.int_(ymax),\
                                          np.int_(xmin):np.int_(xmax)])
-            xcb_subpix, ycb_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
-            print('threshold =',threshold)
+            x_subpix, y_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
+            while len(sources) > 1:
+                threshold += 250
+                daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold) 
+                sources = daofind(image2[np.int_(y-boxsize):np.int_(y+boxsize),np.int_(x-boxsize):np.int_(x+boxsize)])
+                x_subpix, y_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
         except:
             try:
                 threshold=5e2
-                daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
+                #print('threshold =',threshold)
+                daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold) 
                 sources = daofind(image2[np.int_(ymin):np.int_(ymax),\
                                              np.int_(xmin):np.int_(xmax)])
-                xcb_subpix, ycb_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
-                print('threshold =',threshold)
+                x_subpix, y_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
+                while len(sources) > 1:
+                    threshold += 250
+                    daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold) 
+                    sources = daofind(image2[np.int_(y-boxsize):np.int_(y+boxsize),np.int_(x-boxsize):np.int_(x+boxsize)])
+                    x_subpix, y_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
             except:
-                print('I just cant find it, it might be too faint or not there.')
+                print("Failed to find all stars.")
+                x_subpix, y_subpix = np.nan, np.nan
 
-    # If it finds too many sources, keep uping the search threshold until there is only one:
-    while len(sources) > 1:
-        threshold += 500
-        daofind = DAOStarFinder(fwhm=10.0, threshold=threshold) 
-        sources = daofind(image2[np.int_(y-boxsizey):np.int_(y+boxsizey),np.int_(x-boxsizex):np.int_(x+boxsizex)])
-        xcb_subpix, ycb_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
-        
-    return xca_subpix, yca_subpix, xcb_subpix, ycb_subpix
+    return x_subpix, y_subpix
 
-def findstars_in_dataset(dataset_path, xca, yca, corrboxsizex = 40, corrboxsizey = 40, boxsizex = 100, boxsizey = 100, skip_list = False, \
-                         append_file = False, threshold = 1e4, radius = 20):
+def findstars(imstamp, scienceimage_filename, nstars, \
+              boxsize = 100, threshold = 1e4, fwhm = 10, radius = 20):
+    """Find the subpixel location of all stars in a single clio BDI image using DAOStarFinder 
+       (https://photutils.readthedocs.io/en/stable/api/photutils.detection.DAOStarFinder.html).
+
+       Parameters:
+       -----------
+       imstamp : 2d array
+           boxsizex by boxsizey substamp image of a reference psf for cross correlation
+       scienceimage_filename : string
+           path to science image
+       nstars : int
+           number of stars in the image.
+       boxsize : int
+           size of box to draw around star psfs for DAOStarFinder
+       threshold : int
+           threshold keyword for DAO StarFinder
+       fwhm : int
+           fwhm keyword for DAO StarFinder
+       radius : int
+           radius to use when masking stars in image
+           
+       Returns:
+       --------
+       x_subpix, y_subpix : arr,flt
+           1 x nstars array of subpixel x location and y location of stars
+    """
+    from scipy import signal, ndimage
+    from photutils import DAOStarFinder
+    from astropy.io import fits
+    import numpy as np
+    # Open science target image:
+    image = fits.getdata(scienceimage_filename)
+    # If image is a cube, take the first image:
+    if len(image.shape) == 3:
+        image = image[0]
+    # Median filter to smooth image:
+    image = ndimage.median_filter(image, 3)
+    # Use cross-correlation to find int(y,x) of star A (brightest star) in image:
+    corr = signal.correlate2d(image, imstamp, boundary='symm', mode='same')
+    # Find the location of the brightest star in the image:
+    y, x = np.unravel_index(np.argmax(corr), corr.shape)
+    # Make container to hold results:
+    x_subpix, y_subpix = np.array([]), np.array([])
+    # Make a copy of the correlation image to mask:
+    corr_masked = corr.copy()
+    image_masked = image.copy()
+    # For each star in the image:
+    for i in range(nstars):
+        # Use DAO Star Finder to find the subpixel location of the star at that location:
+        xs, ys = daostarfinder(image_masked, x, y, boxsize = boxsize, threshold = threshold, fwhm = fwhm)
+        x_subpix, y_subpix = np.append(x_subpix, xs), np.append(y_subpix, ys)
+        # Make a mask around that star on the cross-correlation image:
+        # Make a meshgrid of the image centered at the last found star:
+        xx,yy = np.meshgrid(np.arange(image.shape[1])-x_subpix[i],np.arange(image.shape[0])-y_subpix[i])
+        # Make an array of the distances of each pixel from that center:
+        rA=np.hypot(xx,yy)
+        # Mask wherever that distance is less than the set radius and
+        # set those pixels to zero:
+        corr_masked[np.where((rA < radius))] = 0
+        image_masked[np.where((rA < radius))] = 0
+        # Now find the new highest correlation which should be the next star:
+        y, x = np.unravel_index(np.argmax(corr_masked), corr.shape)
+        # Repeat until all stars are found.
+
+    return x_subpix, y_subpix
+
+def findstars_in_dataset(dataset_path, nstars, xca, yca, corrboxsizex = 40, corrboxsizey = 40, boxsize = 100, skip_list = False, \
+                         append_file = False, threshold = 1e4, radius = 20, fwhm = 10):
     """Find the subpixel location of stars A and B in a clio BDI dataset.
        Parameters:
        -----------
@@ -118,8 +159,8 @@ def findstars_in_dataset(dataset_path, xca, yca, corrboxsizex = 40, corrboxsizey
            path to science images including image prefixes and underscores.  
            ex: An image set of target BDI0933 with filenames of the form BDI0933__00xxx.fit
                would take as input a path string of 'BDI0933/BDI0933__'
-       xca, yca, xcb, ycb : int
-           integer pixel locations of star A and B in the first image of the dataset, rough guess 
+       xca, yca : int
+           integer pixel locations of a star in the first image of the dataset, rough guess 
        corrrboxsize : int
            size of box to draw around star psfs for imstamp and cross correlation
        boxsize : int
@@ -136,8 +177,8 @@ def findstars_in_dataset(dataset_path, xca, yca, corrboxsizex = 40, corrboxsizey
 
        Returns:
        --------
-       writes subpixel location of star A and star B to file called 'ABlocations' in order
-           xca_subpix, yca_subpix, xcb_subpix, ycb_subpix
+       writes subpixel location of stars to file called 'StarLocations' in order:
+           image_filename   x   y   x   y   ...ect. for all requested stars.
     """
     from scipy import ndimage
     from astropy.io import fits
@@ -146,12 +187,17 @@ def findstars_in_dataset(dataset_path, xca, yca, corrboxsizex = 40, corrboxsizey
     import numpy as np
     # Supress warnings when failing to find point sources
     import warnings
-    #warnings.filterwarnings("ignore")
+    warnings.filterwarnings("ignore")
     # Make a file to store results:
-    newfile = dataset_path.split('/')[0]+'/ABlocations'
+    newfile = dataset_path.split('/')[0]+'/StarLocations'
+    
     if append_file == False:
+        string = '#     '
+        for i in range(nstars):
+            string += 'xc'+str(i+1) + '     ' + 'yc'+str(i+1)  + '     ' 
+        string += "\n"
         k = open(newfile, 'w')
-        k.write('#     xca     yca     xcb     ycb' + "\n")
+        k.write(string)
         k.close()
     
     # Make a list of all images in dataset:
@@ -171,93 +217,23 @@ def findstars_in_dataset(dataset_path, xca, yca, corrboxsizex = 40, corrboxsizey
     count = 0
     for im in ims:
         # For each image in the dataset, find subpixel location of stars:
-        try:
-            xca_subpix, yca_subpix, xcb_subpix, ycb_subpix = findstars(imstamp, im, \
-                                                           xca, yca, \
-                                                           boxsizex = boxsizex, boxsizey = boxsizey, threshold = threshold, \
-                                                           radius = radius)
-            if count % 25 == 0:
-                xca, yca, xcb, ycb = xca_subpix, yca_subpix, xcb_subpix, ycb_subpix
-            string = im + ' ' + str(xca_subpix)+' '+str(yca_subpix)+' '+str(xcb_subpix)+' '+str(ycb_subpix)
-        except:
-            # DAOStarFinder failed to find a point source due to poor image quality.  Make
-            # a comment in the locations file
-            print(im,': failed to find stars')
-            # play beep if failed:
-            os.system('echo -e "\a"')
-            string = '# ' + im + ' ' + str('...')+' '+str('...')+' '+str('...')+' '+str('...')
-        # Write results to file:
+        x_subpix, y_subpix = findstars(imstamp, im, nstars, \
+                                                           boxsize = boxsize, threshold = threshold, \
+                                                           radius = radius, fwhm = fwhm)
+        if any(np.isnan(x_subpix)) or any(np.isnan(y_subpix)):
+            # If any of the stars were failed to find, mark this entry with a comment:
+            string = '# '+ im + ' '
+        else:
+            string = im + ' '
+        for i in range(nstars):
+            string += str(x_subpix[i]) + '     ' + str(y_subpix[i])  + '     ' 
+        string += "\n"
+
         k = open(newfile, 'a')
-        k.write(string + "\n")
+        k.write(string)
         k.close()
-        # update x,y locations:
         
         count+=1
         update_progress(count,len(ims))
     print('Done')
     os.system("say 'done'")
-
-
-def findstars_depreicated(imstamp, scienceimage_filename, xca, yca, xcb, ycb, boxsizex = 60, boxsizey = 60):
-    """Find the subpixel location of stars A and B in a single clio BDI image. **DEPRICATED**
-       Parameters:
-       -----------
-       imstamp : 2d array
-           boxsizex by boxsizey substamp image of a reference psf for cross correlation
-       scienceimage_filename : string
-           path to science image
-       xca, yca, xcb, ycb : int
-           integer pixel locations of star A and B rough guess
-       boxsize : int
-           size of box to draw around star psfs for DAOStarFinder
-           
-       Returns:
-       --------
-       xca_subpix, yca_subpix, xcb_subpix, ycb_subpix : flt
-           subpixel location of star A and star B
-    """
-    from scipy import signal, ndimage
-    from photutils import DAOStarFinder
-    from astropy.io import fits
-    import numpy as np
-    
-    # Open science target image:
-    image2 = fits.getdata(scienceimage_filename)
-    if len(image2.shape) == 3:
-        image2 = image2[0]
-    image2 = ndimage.median_filter(image2, 3)
-    # Use cross-correlation to find int(y,x) of star A (brightest star) in image:
-    corr = signal.correlate2d(image2[:,0:275], imstamp, boundary='symm', mode='same')
-    y, x = np.unravel_index(np.argmax(corr), corr.shape)
-    # Define the star finder parameters:
-    # The settings fwhm = 10.0 threshold = 1e4 are best for distinguishing good psf
-    # from bad psf, by trial and error.
-    daofind = DAOStarFinder(fwhm=10.0, threshold=1e3) 
-    # Find sub-pixel location of star A in science image by using DAOStarFinder on a postagestamp centered at
-    # cross-correlation x,y position results:
-    sources = daofind(image2[np.int_(y-boxsizey):np.int_(y+boxsizey),np.int_(x-boxsizex):np.int_(x+boxsizex)])
-    # Get image location of star A:
-    xca_subpix, yca_subpix = (x-boxsizex+sources['xcentroid'])[0], (y-boxsizey+sources['ycentroid'])[0]
-
-    # Find integer pixel location of B relative to A:
-    deltax, deltay = xcb - xca, ycb - yca
-    
-    # Define box around source B:
-    ymin, ymax = yca_subpix+deltay-boxsizey, yca_subpix+deltay+boxsizey
-    xmin, xmax = xca_subpix+deltax-boxsizex, xca_subpix+deltax+boxsizex
-    # Correct for sources near image edge:
-    if ymin < 0:
-        ymin = 0
-    if ymax > 512:
-        ymax = 512
-    if xmin < 0:
-        xmin = 0
-    if xmax > 1024:
-        xmax = 1024
-    # Use DAOFind to find subpixel location of B in an image stamp centered at A's location plus the
-    # delta pixels of B from A:
-    daofind = DAOStarFinder(fwhm=10.0, threshold=1e3)
-    sources = daofind(image2[np.int_(ymin):np.int_(ymax),\
-                             np.int_(xmin):np.int_(xmax)])
-    xcb_subpix, ycb_subpix = (xmin+sources['xcentroid'])[0], (ymin+sources['ycentroid'])[0]
-    return xca_subpix, yca_subpix, xcb_subpix, ycb_subpix
